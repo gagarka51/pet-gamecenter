00250a41887dfece370bb5ba3dfe860de82d46a5
Svetlana Sharkova <sharkova-2000@mail.ru>
2025-07-08 19:59:53 +0300

    add the ability to move in turn

--- assets/js/tic-tac-toe.js
+++ assets/js/tic-tac-toe.js
@@ -1,11 +1,14 @@
 let squares = document.querySelectorAll(".square");
+let countStep = 0;
 
 squares.forEach((square) => {
 	square.addEventListener("click", function () {
-		if (square.innerHTML === "") {
+		if (square.innerHTML === "" && countStep % 2 == 0) {
 			square.innerHTML = "x";
+			countStep++;
 		} else if (square.innerHTML === "" && square.innerHTML != "x") {
 			square.innerHTML = "o";
+			countStep++;
 		} else {
 			alert("Ой-ой-ой, Бобрито Бандито!");
 		}
